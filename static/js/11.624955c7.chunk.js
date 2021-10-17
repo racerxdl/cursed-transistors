fafefbffd9e4c1386ae0caf4f13369d45710c621
Lucas Teske <lucas@teske.net.br>
2021-10-17 12:47:56 -0300

    Update page UI

--- static/js/11.624955c7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcursedmint=this.webpackJsonpcursedmint||[]).push([[11],{1312:function(n,c){},1319:function(n,c){},976:function(n,c){}}]);
-//# sourceMappingURL=11.624955c7.chunk.js.map
--- static/js/12.cac11314.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcursedmint=this.webpackJsonpcursedmint||[]).push([[12],{1373:function(n,c){},1443:function(n,c){},976:function(n,c){}}]);
-//# sourceMappingURL=12.cac11314.chunk.js.map
