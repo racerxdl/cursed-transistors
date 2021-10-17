f9021d51046a4d866b0caf0d5c26ffedfbd4ff4e
Lucas Teske <lucas@teske.net.br>
2021-10-17 17:20:27 -0300

    Add production page

---
+++ static/js/11.0edcfb49.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcursedmint=this.webpackJsonpcursedmint||[]).push([[11],{1314:function(n,c){},1321:function(n,c){},978:function(n,c){}}]);
+//# sourceMappingURL=11.0edcfb49.chunk.js.map
---
+++ static/js/22.175338e2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcursedmint=this.webpackJsonpcursedmint||[]).push([[22],{1456:function(s,n){}}]);
+//# sourceMappingURL=22.175338e2.chunk.js.map
